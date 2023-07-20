7def22a31e945ed9b111ee5a2087b50de0bc2f03
Abril Da Pra <abydapra@gmail.com>
2023-07-20 16:23:40 -0300

    Declaro el objeto fuera de la función

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Creo el array para el shopping cart
 let cart = [];
 
+//Creo un objeto para el producto
+let product = {
+    name: name,
+    price: price,
+    quantity: quantity
+};
+
 //Creo una función para que el usuario pueda agregar un producto al carrito
 function addToCart(){
     let name = prompt('Ingresá el nombre del producto:');
     let price = parseInt(prompt('Ingresá el precio del producto'));
     let quantity = parseInt(prompt('Ingresá la cantidad que querés comprar:'));
-
-    //Creo un objeto para el producto
-    let product = {
-        name: name,
-        price: price,
-        quantity: quantity
-    };
 
     //Para agregar el producto ingresado al carrito
     cart.push(product);
@@ -23,6 +23,9 @@ function addToCart(){
     //Muestro el objeto de producto actualizado
     console.log(product);
 }
+
+addToCart();
+
 
 
 
