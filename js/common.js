f3bd8d7bdfd82301da8f99db942b20a333a852cc
Abril Da Pra <abydapra@gmail.com>
2023-08-15 21:06:49 -0300

    Agrego event listener para el botón submit del form, toastify y await async

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 //Obtengo el valor actual del contador desde el almacenamiento local
 let cartItemCount = localStorage.getItem("cartItemCount");
-
+//cart?
 
 cartItemCounter.innerText = cartItemCount || 0; 
 
